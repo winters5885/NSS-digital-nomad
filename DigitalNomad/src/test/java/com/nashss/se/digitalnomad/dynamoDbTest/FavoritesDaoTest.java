aa4344ba037656a4d2df70d68db68bf9ca683492
Alex Murillo <alex.d.murillo@protonmail.com>
2022-11-30 11:48:50 -0600

    FavoritesDaoTest half written

--- DigitalNomad/src/test/java/com/nashss/se/digitalnomad/dynamoDbTest/FavoritesDaoTest.java
+++ DigitalNomad/src/test/java/com/nashss/se/digitalnomad/dynamoDbTest/FavoritesDaoTest.java
@@ -1,58 +1,68 @@
-//package com.nashss.se.digitalnomad.dynamoDbTest;
+package com.nashss.se.digitalnomad.dynamoDbTest;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.nashss.se.digitalnomad.dynamoDb.FavoritesDao;
+import com.nashss.se.digitalnomad.dynamoDb.models.Destination;
+import com.nashss.se.digitalnomad.dynamoDb.models.Favorite;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class FavoritesDaoTest {
+
+    @Mock
+    private DynamoDBMapper dynamoDBMapper;
+    private FavoritesDao favoritesDao;
+
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        favoritesDao = new FavoritesDao(dynamoDBMapper);
+    }
+
+    @Test
+    void saveFavorites_validSaveFavoritesRequest_returnsSaveFavoritesRequest() {
+        //GIVEN
+        String userId = "testUser";
+        List<String> favoriteDestinations = new ArrayList<>();
+        String destination1 = "destination1";
+        String destination2 = "destination2";
+        favoriteDestinations.add(destination1);
+        favoriteDestinations.add(destination2);
+
+        Favorite favorite = new Favorite(userId, favoriteDestinations);
+
+        when(dynamoDBMapper.save(eq(Favorite.class)).thenReturn(favorite);
+
+
 //
-//import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-//import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
-//import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
-//import com.nashss.se.digitalnomad.dynamoDb.DestinationsDao;
-//import com.nashss.se.digitalnomad.dynamoDb.models.Destination;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.ArgumentCaptor;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class FavoritesDaoTest {
-//
-//    @Mock
-//    private DynamoDBMapper dynamoDBMapper;
-//    private DestinationsDao destinationsDao;
-//
-//    @Mock
-//    private PaginatedQueryList<Destination> queryResult;
-//
-//
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.openMocks(this);
-//        destinationsDao = new DestinationsDao(dynamoDBMapper);
-//    }
-//
-//    @Test
-//    void getDestinations_QueriesDynamoDbDestinationsTable_returnsDestinations() {
-//        //GIVEN
-//        String category = "nightlife";
-//        when(dynamoDBMapper.query(eq(Destination.class), any())).thenReturn(queryResult);
 //
 //        ArgumentCaptor<DynamoDBQueryExpression> queryExpressionArgumentCaptor =
 //                ArgumentCaptor.forClass(DynamoDBQueryExpression.class);
-//
-//        // WHEN
-//        List<Destination> destinations = destinationsDao.getDestinations(category);
-//
-//        // THEN
+
+        // WHEN
+       Favorite result = favoritesDao.saveFavorites(favorite);
+
+        // THEN
 //        verify(dynamoDBMapper).query(eq(Destination.class), queryExpressionArgumentCaptor.capture());
 //        DynamoDBQueryExpression queryExpression = queryExpressionArgumentCaptor.getValue();
 //
 //        verify(dynamoDBMapper).query(Destination.class, queryExpression);
-//
+
+
+        verify(dynamoDBMapper).save(eq(Favorite.class));
 //        assertEquals(queryResult, destinations, "Expected method to return the results of the query");
-//    }
-//}
+    }
+}
